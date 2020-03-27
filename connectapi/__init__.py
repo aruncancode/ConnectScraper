2e99ddc524990fcd218e4298f8b3427877fc32b9
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2020-03-27 17:03:32 +0800

    Collapsed really long string

--- connectapi/__init__.py
+++ connectapi/__init__.py
@@ -15,7 +15,16 @@ class ConnectApi:
     def login(self):
         with Session() as self.__s:
             site = self.__s.get(
-                "https://login.det.wa.edu.au/oam/server/obrareq.cgi?encquery%3DbPKvnLG4zJaQiON3%2Bp9vQkDuz5KpPGozevLjKgY76eSGfwKkjlotkknV5QNphwEFWqSgsivKQRpo9gNd8XvGj960VAV5X8UAGJMzW%2Fk6LoorLp2kg3y2ltk0PunbQh%2FLePCIXxtDBX3OX07vYS6soVgDPkJzNl%2Btc%2B1uwXqb8P3LaP3mJkwVx2XVCf3AI7N6Pc751JpS9ZWcD1O8pboDrNrLefBNSjS9arWkQ0Z8QcXHAHTn9rii3eBCMEzvdKj2RtLC%2FMiUQvdtM5TsBtreZlrwR50xkss3JkoKStatb9Q3h%2BeGoP3ZB5shYKx7%2B4owl7sN%2FAiLdqnI84y8lB9Z3%2B2wKBgifCRvdC20Aw11g%2Bpf%2BiezpmVDQgQJP3vUypjl%20agentid%3DCONNECT%20ver%3D1%20crmethod%3D2&ECID-Context=1.97289976909130380%3BkXhglXjE"
+                "https://login.det.wa.edu.au/oam/server/obrareq.cgi?encque"
+                "ry%3DbPKvnLG4zJaQiON3%2Bp9vQkDuz5KpPGozevLjKgY76eSGfwKkjl"
+                "otkknV5QNphwEFWqSgsivKQRpo9gNd8XvGj960VAV5X8UAGJMzW%2Fk6L"
+                "oorLp2kg3y2ltk0PunbQh%2FLePCIXxtDBX3OX07vYS6soVgDPkJzNl%2"
+                "Btc%2B1uwXqb8P3LaP3mJkwVx2XVCf3AI7N6Pc751JpS9ZWcD1O8pboDr"
+                "NrLefBNSjS9arWkQ0Z8QcXHAHTn9rii3eBCMEzvdKj2RtLC%2FMiUQvdt"
+                "M5TsBtreZlrwR50xkss3JkoKStatb9Q3h%2BeGoP3ZB5shYKx7%2B4owl"
+                "7sN%2FAiLdqnI84y8lB9Z3%2B2wKBgifCRvdC20Aw11g%2Bpf%2Biezpm"
+                "VDQgQJP3vUypjl%20agentid%3DCONNECT%20ver%3D1%20crmethod%3"
+                "D2&ECID-Context=1.97289976909130380%3BkXhglXjE"
             )
             bsContent = bs(site.content, "html.parser")
             hiddenStuff = {
