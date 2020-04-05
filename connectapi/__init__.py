f6c11ecabdb06fa523282988e6deaa1bfb95e538
urwrstkn8mare <42769125+urwrstkn8mare@users.noreply.github.com>
2020-04-05 18:58:59 +0800

    Overhauled and switch to Selenium!

--- connectapi/__init__.py
+++ connectapi/__init__.py
@@ -1,70 +1,46 @@
-from requests import Session
-from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+CHROME_DRIVER_PATH = None
 
 
 class ConnectApi:
-    def __init__(self, username: str, password: str):
+    def __init__(self, username: str, password: str, *, headless=True):
         if not isinstance(username, str):
             raise TypeError("The username parameter must be a string.")
         if not isinstance(password, str):
             raise TypeError("The password parameter must be a string.")
         self.__username = username
         self.__password = password
-        self.__s = None
+        global CHROME_DRIVER_PATH
+        if CHROME_DRIVER_PATH is None:
+            import chromedriver_autoinstaller
+
+            CHROME_DRIVER_PATH = chromedriver_autoinstaller.install(cwd=True)
+        # The browser will close when it is garbage collected by the
+        # interpreter. This shouldn't be a problem since it will stay
+        # open as long as you are using it.
+        if headless:
+            from selenium.webdriver.chrome.options import Options
+
+            options = Options()
+            options.add_argument("--disable-gpu")
+            options.add_argument("--headless")
+            options.add_argument("--hide-scrollbars")
+            options.add_argument("--log-level=3")  # fatal
+            self.browser = webdriver.Chrome(
+                executable_path=CHROME_DRIVER_PATH, options=options
+            )
+        else:
+            self.browser = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
 
     def login(self):
-        with Session() as self.__s:
-            site = self.__s.get(
-                "https://login.det.wa.edu.au/oam/server/obrareq.cgi?encque"
-                "ry%3DbPKvnLG4zJaQiON3%2Bp9vQkDuz5KpPGozevLjKgY76eSGfwKkjl"
-                "otkknV5QNphwEFWqSgsivKQRpo9gNd8XvGj960VAV5X8UAGJMzW%2Fk6L"
-                "oorLp2kg3y2ltk0PunbQh%2FLePCIXxtDBX3OX07vYS6soVgDPkJzNl%2"
-                "Btc%2B1uwXqb8P3LaP3mJkwVx2XVCf3AI7N6Pc751JpS9ZWcD1O8pboDr"
-                "NrLefBNSjS9arWkQ0Z8QcXHAHTn9rii3eBCMEzvdKj2RtLC%2FMiUQvdt"
-                "M5TsBtreZlrwR50xkss3JkoKStatb9Q3h%2BeGoP3ZB5shYKx7%2B4owl"
-                "7sN%2FAiLdqnI84y8lB9Z3%2B2wKBgifCRvdC20Aw11g%2Bpf%2Biezpm"
-                "VDQgQJP3vUypjl%20agentid%3DCONNECT%20ver%3D1%20crmethod%3"
-                "D2&ECID-Context=1.97289976909130380%3BkXhglXjE"
-            )
-            bsContent = bs(site.content, "html.parser")
-            hiddenStuff = {
-                "site2pstoretoken": bsContent.find(
-                    "input", {"name": "site2pstoretoken"}
-                )["value"],
-                "OAM_REQ": bsContent.find("input", {"name": "OAM_REQ"})[
-                    "value"
-                ],
-                "request_id": bsContent.find("input", {"name": "request_id"})[
-                    "value"
-                ],
-            }
-            loginData = {
-                "site2pstoretoken": hiddenStuff["site2pstoretoken"],
-                "OAM_REQ": hiddenStuff["OAM_REQ"],
-                "request_id": hiddenStuff["request_id"],
-                "ssousername": self.__username,
-                "password": self.__password,
-                "acceptterms": "acceptterms",
-                "submit": "Login",
-            }
-            self.__s.post(
-                "https://login.det.wa.edu.au/oam/server/auth_cred_submit",
-                loginData,
-            )
-
-    def getCustomPage(self, url: str, *, raw=False):
-        if self.__s is None:
-            raise Exception(
-                "You must login to connect first using the login() method!"
-            )
-        if not isinstance(url, str):
-            raise TypeError("The url parameter must be a string!")
-        if not isinstance(raw, bool):
-            raise TypeError("The raw parameter must be a bool!")
-
-        page = self.__s.get(url)
-
-        if raw:
-            return page.text
-        else:
-            return bs(page.content, "html.parser")
+        self.browser.get("https://connect.det.wa.edu.au/")
+        usernameBox = self.browser.find_element(By.ID, "ssousername")
+        usernameBox.send_keys(self.__username)
+        passwordBox = self.browser.find_element(By.ID, "password")
+        passwordBox.send_keys(self.__password)
+        checkBox = self.browser.find_element(By.NAME, "acceptterms")
+        checkBox.click()
+        loginButton = self.browser.find_element(By.ID, "login")
+        loginButton.click()
--- test.py
+++ test.py
@@ -1,15 +1,12 @@
 from connectapi import ConnectApi
 import json
+import time
 
 f = open("sensitiveInfo.json", "r")
 sensitiveInfo = json.loads(f.read())
 f.close()
 connect = ConnectApi(sensitiveInfo["username"], sensitiveInfo["password"])
 connect.login()
-f = open("output.html", "w")
-f.write(
-    connect.getCustomPage(
-        "https://connect.det.wa.edu.au/group/students/ui/overview", raw=True
-    )
-)
-f.close()
+# After this is done the python garbage collector will automatically
+# close the browser.
+time.sleep(10)
